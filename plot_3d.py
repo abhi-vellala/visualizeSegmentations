6d37079a2b7b231018a1640ac2cd0c97c2c6447f
abhi-vellala <abhivellala@gmail.com>
2023-10-14 17:53:25 +0200

    adding 3d plot

---
+++ plot_3d.py
@@ -0,0 +1,14 @@
+# from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+import nibabel as nib
+
+mask_load = nib.load('./data/covid_infection_mask.nii')
+mask = mask_load.get_fdata()
+mask = mask.T
+z, x, y = mask.nonzero()
+
+# fig = plt.figure(figsize=(15,8))
+ax = plt.axes(projection='3d')
+
+ax.scatter(x, y, z, c=z, alpha=0.05)
+plt.show()
